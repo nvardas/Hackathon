3ce997c0a3519775e1095c47d48be5628e887e6b
Nathan Vardas <vard6246@eduhsd.k12.ca.us>
2017-05-13 10:51:28 -0700

    Added dictionary file to project.

---
+++ Dictionary.java
@@ -0,0 +1,43 @@
+public class Dictionary {
+    private static String[][] possibleNouns;
+    private static String[][] possibleVerbs;
+    
+    public Dictionary() {
+        possibleNouns = new String[26];
+        
+        possibleNouns[0] = {"Apple", "Aardvark", "Algorithm", "Alien", "Alpaca", "Amazon", "Albino", "Accountant", "Alex", "Alligator"};
+        possibleNouns[1] = {"Banana", "Buffoon", "Boat", "Biscuit", "Bathtub", "Britain", "Backpack", "Box", "Baby", "Bale"};
+        possibleNouns[2] = {"Calvin", "Crepe", "Crayon", "Cardboard", "Concrete", "Carrot", "Computer", "Cat", "Cookie", "Cobra"};
+        possibleNouns[3] = {"Dandelion", "Dress", "Dingo", "Disk", "Dealer", "Deposit", "Dandruff", "Drone", "Dinner", "Database"};
+        possibleNouns[4] = {"Elephant", "Eskimo", "Economy", "Eel", "Exercise", "Earth", "Eye", "Ear", "Eva", "East"};
+        possibleNouns[5] = {"Flamingo", "Fridge", "Flan", "Febreeze", "February", "Freak", "Fun", "France", "Face", "Family"};
+        possibleNouns[6] = {"Georgia", "Greece", "Gallon", "Game", "Garbage", "Gentleman", "Germ", "Gift", "Glass", "Gold"};
+        possibleNouns[7] = {"Hands", "Hawaii", "Hostage", "Hater", "Hacker", "Half", "Hammock", "Handstand", "Handiwork", "Handgun"};
+        possibleNouns[8] = {"Ice", "Idiom", "Idol", "Illustrator", "Image", "Impulse", "Immigrant", "Ingredient", "Insect", "Igloo"};
+        possibleNouns[9] = {"Jacket", "Jail", "Jar", "Joke", "Jet", "Judge", "Juice", "Joy", "Jury", "Junior"};
+        possibleNouns[10] = {"Key", "Kid", "King", "Knife", "Knuckle", "Knowledge", "Kitchen", "Knee", "Kingdom", "Knot"};
+        possibleNouns[11] = {"Lizard", "Lion", "Lab", "Leg", "Ladder", "Lady", "Land", "Language", "Leaf", "Lecture"};
+        possibleNouns[12] = {"Machine", "Magazine", "Magic", "Mountain", "Man", "Map", "Manager", "Master", "Matter", "Maximum"};
+        possibleNouns[13] = {"Nathan", "Name", "Navy", "Neck", "Nation", "Nail", "Neighbor", "Night", "Nerve", "Niece"};
+        possibleNouns[14] = {"Object", "Oath", "Ogre", "Odor", "Offer", "Ocean", "Oil", "Office", "Official", "One"};
+        possibleNouns[15] = {"Pain", "Package", "Paragraph", "Pad", "Panda", "Partner", "Python", "Patient", "Passenger", "Party"};
+        possibleNouns[16] = {"Quote", "Quiz", "Question", "Quotient", "Quarterback", "Quarter", "Queen", "Quiver", "Quilt", "Quinoa"};
+        possibleNouns[17] = {"Rain", "Radiator", "Rail", "Radiation", "Rachel", "Rash", "Refridgerator", "Reference", "Religion", "Republic"};
+        possibleNouns[18] = {"Snake", "Snail", "Sandwich", "Salt", "Stream", "Steam", "Starburst", "Salmon", "Salad", "Sake"};
+        possibleNouns[19] = {"Table", "Tail", "Tums", "Tentacle", "Tank", "Target", "Tundra", "Tea", "Telephone", "Temple"};
+        possibleNouns[20] = {"Uniform", "Umbrella", "Underdog", "Unit", "University", "Universe", "Upstairs", "User", "Uganda", "Union"};
+        possibleNouns[21] = {"Vendetta", "Vent", "Victory", "Video", "Victim", "Vehicle", "Vegetable", "Valley", "Violence", "Vandalism"};
+        possibleNouns[22] = {"Walrus", "West", "Waitress", "Waiter", "Wallet", "War", "Wave", "Water", "Waste", "Wax"};
+        possibleNouns[23] = {"X-Ray", "Xylophone", "Xenon", "Xerxes", "Xerox", "Xylem", "Xavier", "Xenophobe", "Xenomorph", "X-Files"};
+        possibleNouns[24] = {"Yellow", "Yard", "Yesterday", "Youth", "Year", "Yogurt", "Yodle", "Yttriu"m", "Yearling", "Yeast"};
+        possibleNouns[25] = {"Zygote", "Zebra", "Zoo", "Zoologist", "Zombie", "Zipper", "Zoology", "Zuchini", "Zodiac", "Zero"};
+        
+        
+        
+        
+    }
+    
+    
+    
+    public static getPossibleNouns()
+}
